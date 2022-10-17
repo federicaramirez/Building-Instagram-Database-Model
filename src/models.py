1b4fd0b7c96990fa5863a2c8a78d3d56525201e1
federicaramirez <federicaaramirez@gmail.com>
2022-10-17 16:43:25 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,65 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    firstname = Column(String, nullable=False)
+    lastname = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    follower = relationship("Follower", uselist=False, backref="user")
+    post = relationship("Post", uselist=False, backref="user")
+    comment = relationship("Comment", uselist=False, backref="user")
+
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id= Column(Integer, ForeignKey('user.id'), nullable=False)
+    media_id= Column(Integer, ForeignKey('user.id'), nullable=False)
+    comment = relationship("Comment", uselist=False, backref='post')
+    media = relationship("Media", uselist=False, backref='post')
+
+class Comment(Base): 
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250), nullable=False)
+
+    autor_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id =  Column(Integer, ForeignKey('post.id'), nullable=False)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type_media = Column(Stringe, nullable=False)
+    url = Column(Stringe, nullable=False)
+    
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+
+
+
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     Here we define columns for the table address.
+#     Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+#     def to_dict(self):
+#         return {}
 
 ## Draw from SQLAlchemy base
 try:
