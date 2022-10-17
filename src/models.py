af2167089220bd274554d502d1a2b88169108976
federicaramirez <federicaaramirez@gmail.com>
2022-10-17 18:07:20 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -51,24 +51,6 @@ class Media(Base):
     post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
 
-
-
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     Here we define columns for the table address.
-#     Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
-
-#     def to_dict(self):
-#         return {}
-
-## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
